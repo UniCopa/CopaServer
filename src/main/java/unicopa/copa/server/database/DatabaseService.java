ced651364cae9086f797a4089eaaa9b10adac6cb
Felix Wiemuth <felixwiemuth@hotmail.de>
2013-05-23 20:18:07 +0200

    class to provide database access

---
+++ src/main/java/unicopa/copa/server/database/DatabaseService.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2013 UniCopa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package unicopa.copa.server.database;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.jdbc.ScriptRunner;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import unicopa.copa.server.database.util.DatabaseUtil;
+
+/**
+ * The database service provides an interface to the database. It allows to
+ * obtain objects from and write objects to the database.
+ * 
+ * @author Felix Wiemuth
+ */
+public class DatabaseService {
+
+    public static class ObjectNotFoundException extends Exception {
+
+	public ObjectNotFoundException(String message) {
+	    super(message);
+	}
+    }
+
+    public static class ItemNotFoundException extends Exception {
+
+	public ItemNotFoundException(String message) {
+	    super(message);
+	}
+    }
+
+    private static final String RESOURCE_SQL_INITDB = "/nipgm/resources/sql/initializeDB.sql";
+    private static final String RESOURCE_MYBATIS_CONFIG = "nipgm/resources/mybatis-config.xml";
+    private final File database;
+    private SqlSessionFactory sqlSessionFactory; // The session factory used to
+						 // obtain SQL sessions in
+						 // service methods
+
+    /**
+     * Create a new database service. Note: The database must already be
+     * initialized by calling static method 'initDB()'.
+     * 
+     * @param database
+     * @param username
+     * @param password
+     * @throws IOException
+     */
+    public DatabaseService(File database, String username, String password)
+	    throws IOException {
+	this.database = database;
+
+	Properties properties = new Properties();
+	properties.setProperty("username", username);
+	properties.setProperty("password", password);
+	properties.setProperty("url",
+		DatabaseUtil.protocol + database.getCanonicalPath());
+
+	InputStream inputStream = Resources
+		.getResourceAsStream(RESOURCE_MYBATIS_CONFIG);
+	sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream,
+		properties);
+    }
+
+    public DatabaseService(File database) throws IOException {
+	this(database, "", "");
+    }
+
+    /**
+     * 
+     * @param database
+     * @throws unicopa.copa.server.database.util.DatabaseUtil.ConnectException
+     * @throws FileNotFoundException
+     * @throws SQLException
+     * @throws IOException
+     */
+    public static void initDB(File database)
+	    throws DatabaseUtil.ConnectException, FileNotFoundException,
+	    SQLException, IOException {
+	try (Connection conn = DatabaseUtil.getConnection(database);
+		Reader reader = new BufferedReader(new InputStreamReader(
+			DatabaseService.class
+				.getResourceAsStream(RESOURCE_SQL_INITDB)))) {
+	    ScriptRunner runner = new ScriptRunner(conn);
+	    runner.runScript(reader);
+	}
+	// TODO do without DatabaseService?
+	DatabaseService dbservice = new DatabaseService(database);
+	dbservice.insertProgramVersion();
+	// TODO need to shut down DatabaseService?
+    }
+
+    /**
+     * Add the program version to the database.
+     */
+    private void insertProgramVersion() {
+	throw new UnsupportedOperationException();
+    }
+}
