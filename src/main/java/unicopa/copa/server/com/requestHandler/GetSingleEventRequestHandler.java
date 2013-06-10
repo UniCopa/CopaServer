a806bd21c861fcf8768dae22d1e1ddb0584f8869
Felix Wiemuth <felixwiemuth@hotmail.de>
2013-06-10 21:33:16 +0200

    first example request handler plus test

---
+++ src/main/java/unicopa/copa/server/com/requestHandler/GetSingleEventRequestHandler.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2013 UniCoPA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package unicopa.copa.server.com.requestHandler;
+
+import java.util.Date;
+import unicopa.copa.base.com.AbstractRequest;
+import unicopa.copa.base.com.AbstractResponse;
+import unicopa.copa.base.com.GetSingleEventRequest;
+import unicopa.copa.base.com.GetSingleEventResponse;
+import unicopa.copa.base.com.exception.InternalErrorException;
+import unicopa.copa.base.com.exception.PermissionException;
+import unicopa.copa.base.com.exception.RequestNotPracticableException;
+import unicopa.copa.base.event.SingleEvent;
+import unicopa.copa.server.CopaSystemContext;
+
+/**
+ * 
+ * @author Felix Wiemuth
+ */
+public class GetSingleEventRequestHandler extends RequestHandler {
+
+    public GetSingleEventRequestHandler(CopaSystemContext context) {
+	super(context);
+    }
+
+    // TODO this is only a test
+    @Override
+    public AbstractResponse handleRequest(AbstractRequest request)
+	    throws RequestNotPracticableException, InternalErrorException,
+	    PermissionException {
+	GetSingleEventRequest req = (GetSingleEventRequest) request;
+	if (req.getSingleEventID() == 42) {
+	    throw new RequestNotPracticableException(
+		    "Don´t know what to do with 42!?");
+	} else if (req.getSingleEventID() == 0) {
+	    throw new InternalErrorException(
+		    "Haha maybe I try to devide by zero...");
+	} else if (req.getSingleEventID() < 0) {
+	    throw new PermissionException(
+		    "You are not allowed to ask for negative events!");
+	}
+	getContext().getNotifier().notifyClients(null);
+	return new GetSingleEventResponse(new SingleEvent(
+		req.getSingleEventID(), 100, "Room" + req.getSingleEventID(),
+		new Date(), "sup XYZ", 90));
+    }
+
+}
---
+++ src/test/java/unicopa/copa/server/ClientSimulationTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2013 UniCoPA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package unicopa.copa.server;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import unicopa.copa.base.com.AbstractRequest;
+import unicopa.copa.base.com.AbstractResponse;
+import unicopa.copa.base.com.GetSingleEventRequest;
+import unicopa.copa.base.com.GetSingleEventResponse;
+
+/**
+ * Simulate client requests to the service. All requests should be tested here.
+ * 
+ * @author Felix Wiemuth
+ */
+public class ClientSimulationTest {
+
+    private CopaSystem system;
+
+    public ClientSimulationTest() {
+    }
+
+    @Before
+    public void setUp() {
+	system = new CopaSystem();
+	// TODO set up test database (or just use test database below,
+	// but content must be known and guaranteed)
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test GetSingleEventRequest.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testGetSingleEventRequest() throws Exception {
+	AbstractRequest req = new GetSingleEventRequest(1);
+	String send = req.serialize();
+	String recv = system.processClientMessage(send);
+	AbstractResponse resp = AbstractResponse.deserialize(recv);
+	GetSingleEventResponse response = (GetSingleEventResponse) resp;
+	// TODO assert equals
+    }
+}
