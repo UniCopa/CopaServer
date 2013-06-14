41ae14c0fda0e5cfd5a6ec03a6ea5dcaa8aa71f6
Felix Wiemuth <felixwiemuth@hotmail.de>
2013-06-14 12:22:28 +0200

    implemented GetUserSettingsRequestHandler

---
+++ src/main/java/unicopa/copa/server/com/requestHandler/GetUserSettingsRequestHandler.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2013 UniCoPA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package unicopa.copa.server.com.requestHandler;
+
+import unicopa.copa.base.com.exception.InternalErrorException;
+import unicopa.copa.base.com.exception.PermissionException;
+import unicopa.copa.base.com.exception.RequestNotPracticableException;
+import unicopa.copa.base.com.request.AbstractRequest;
+import unicopa.copa.base.com.request.AbstractResponse;
+import unicopa.copa.base.com.request.GetUserSettingsRequest;
+import unicopa.copa.base.com.request.GetUserSettingsResponse;
+import unicopa.copa.server.CopaSystemContext;
+import unicopa.copa.server.database.ObjectNotFoundException;
+
+/**
+ * 
+ * @author Felix Wiemuth
+ */
+public class GetUserSettingsRequestHandler extends RequestHandler {
+
+    public GetUserSettingsRequestHandler(CopaSystemContext context) {
+	super(context);
+    }
+
+    @Override
+    public AbstractResponse handleRequest(AbstractRequest request, int userID) throws PermissionException, RequestNotPracticableException, InternalErrorException {
+        GetUserSettingsRequest req = (GetUserSettingsRequest) request;
+        try {
+            return new GetUserSettingsResponse(getContext().getDbservice().getUserSettings(userID));
+        } catch(ObjectNotFoundException ex) {
+            throw new RequestNotPracticableException("The user with the specified ID could not be found.");
+        }
+    }
+}
